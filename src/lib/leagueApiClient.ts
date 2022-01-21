333ea76d2c1b89774c6c05cceef26ac27cf15ec5
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-01-21 22:37:43 +0100

    prepare match endpoints

--- src/lib/leagueApiClient.ts
+++ src/lib/leagueApiClient.ts
@@ -1,5 +1,41 @@
 export const apiKey = process.env.NEXT_PUBLIC_RIOT_GAMES_API_KEY || 'API_KEY_MISSING'
 
+// TODO: add matchIds query
+// https://developer.riotgames.com/apis#match-v5/
+export const getMatchIds = async (puuid: string) => {
+	try {
+		const matchIdsResponse = await fetch(`https://euw1.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids`, {
+			headers: {
+				'X-Riot-Token': apiKey,
+			},
+		})
+		if (matchIdsResponse.status === 429) {
+			throw new Error('Rate limit exceeded')
+		}
+		return (await matchIdsResponse.json()) as string[]
+	} catch (e) {
+		console.error(`Error fetching Match Ids by puuid: ${puuid}: ${e}`)
+	}
+}
+
+// https://developer.riotgames.com/apis#match-v5/
+export const getMatch = async (matchId: string) => {
+	try {
+		const matchResponse = await fetch(`https://euw1.api.riotgames.com/lol/match/v5/matches/${matchId}`, {
+			headers: {
+				'X-Riot-Token': apiKey,
+			},
+		})
+		if (matchResponse.status === 429) {
+			throw new Error('Rate limit exceeded')
+		}
+		return await matchResponse.json()
+	} catch (e) {
+		console.error(`Error fetching Matches by id: ${matchId}: ${e}`)
+	}
+}
+
+// https://developer.riotgames.com/apis#summoner-v4
 export const getSummoner = async (summonerName: string) => {
 	try {
 		const summonerDataResponse = await fetch(
@@ -19,6 +55,7 @@ export const getSummoner = async (summonerName: string) => {
 	}
 }
 
+// https://developer.riotgames.com/apis#league-v4
 export const getEntries = async (summonerId: string) => {
 	try {
 		const summonerEntriesResponse = await fetch(
