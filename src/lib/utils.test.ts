3f3c1090537900b05d31be3bd6dd546d1e518151
Patrick Obermueller <pat.obermueller@gmail.com>
2022-02-26 13:33:28 +0100

    add filterRankedSolo5x5 test with no entries as input

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,4 +1,4 @@
-import { mapRankToPoints } from './utils'
+import { mapRankToPoints, filterRankedSolo5x5 } from './utils'
 
 describe('::: utils.ts :::', () => {
 	it('mapRankToPoints: with valid ranks', () => {
@@ -7,5 +7,14 @@ describe('::: utils.ts :::', () => {
 		expect(mapRankToPoints('III')).toBe(200)
 		expect(mapRankToPoints('IV')).toBe(100)
 		expect(mapRankToPoints('')).toBe(0)
+	})
+
+	it('filterRankedSolo5x5: with no entries', () => {
+		expect(filterRankedSolo5x5(null)).toBe(null)
+		expect(filterRankedSolo5x5([])).toBe(null)
+	})
+
+	it('filterRankedSolo5x5: with entries', () => {
+		// TODO: add missing tests with filters of entries
 	})
 })
